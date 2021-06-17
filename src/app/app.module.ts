7de0ad7957ce86b48c17cf6ab35921dde5f557fa
Rana <rana.potu@gmail.com>
2021-06-17 22:56:45 +0100

    add3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './material/material.module';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, ContentComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
