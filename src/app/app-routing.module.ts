01eceb067160c01d0acd1025de7c08fb0ac4c350
Rana <rana.potu@gmail.com>
2021-06-18 19:48:31 +0100

    add5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './blog/blog.component';
 import { ContactComponent } from './contact/contact.component';
 import { ContentComponent } from './content/content.component';
+import { RecentProjectsComponent } from './recent-projects/recent-projects.component';
 import { WorkComponent } from './work/work.component';
 
 const routes: Routes = [
   { path: 'about', component: ContentComponent },
+  { path: 'recent-projects', component: RecentProjectsComponent },
   { path: 'work', component: WorkComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'blog', component: BlogComponent },
@@ -19,6 +21,7 @@ const routes: Routes = [
 export class AppRoutingModule {}
 export const routingComponents = [
   ContentComponent,
+  RecentProjectsComponent,
   WorkComponent,
   ContactComponent,
   BlogComponent,
