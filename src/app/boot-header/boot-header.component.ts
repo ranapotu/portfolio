284730a9426b52c558a728c5ff4c59bd3a1bc313
Rana <rana.potu@gmail.com>
2021-06-22 01:00:14 +0100

    p3

--- src/app/boot-header/boot-header.component.ts
+++ src/app/boot-header/boot-header.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-boot-header',
   templateUrl: './boot-header.component.html',
   styleUrls: ['./boot-header.component.css'],
+  host: {
+    '(document:click)': 'onClick($event)',
+  },
 })
 export class BootHeaderComponent implements OnInit {
   isOpen: boolean = false;
-  constructor() {}
+  constructor(private _eref: ElementRef) {}
 
   ngOnInit(): void {}
+  onClick(event: any) {
+    if (!this._eref.nativeElement.contains(event.target))
+      // or some similar check
+      this.isOpen = false;
+  }
 }
