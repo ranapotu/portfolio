9c07602aca15d1546df4683d0e4c6a70dd96748c
Rana <rana.potu@gmail.com>
2021-06-22 01:32:44 +0100

    p4

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { ContactComponent } from '../contact/contact.component';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(ContactComponent);
+  }
 
   ngOnInit(): void {}
 }
