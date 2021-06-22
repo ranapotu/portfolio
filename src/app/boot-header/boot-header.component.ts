586e19f14363041fb767ae0b512a0069b3d903a2
Rana <rana.potu@gmail.com>
2021-06-22 18:58:36 +0100

    p7

--- src/app/boot-header/boot-header.component.ts
+++ src/app/boot-header/boot-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-boot-header',
@@ -10,12 +12,16 @@ import { Component, ElementRef, OnInit } from '@angular/core';
 })
 export class BootHeaderComponent implements OnInit {
   isOpen: boolean = false;
-  constructor(private _eref: ElementRef) {}
+  constructor(private _eref: ElementRef, public dialog: MatDialog) {}
 
   ngOnInit(): void {}
   onClick(event: any) {
     if (!this._eref.nativeElement.contains(event.target))
       // or some similar check
       this.isOpen = false;
+  }
+
+  openDialog() {
+    this.dialog.open(ContactComponent);
   }
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openDialog() {
+    this.dialog.open(ContactComponent);
   }
-
 }
